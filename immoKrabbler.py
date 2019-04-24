7a0b1f5af78f13631196b518cf21c8c5911c98f4
HeLiBlocks <hkel81@gmail.com>
2019-04-24 21:03:51 +0200

    added license

--- immoKrabbler.py
+++ immoKrabbler.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# immoKrabbler.py crawl objects from immobilienscout24 and save them to a RDMS
+# Copyright © 2019 Henrik Lindgren (henrikprojekt at googlemail dot com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table
